741167e8a70e38e5fa75a80990ef943099935cff
Sebastian <a20206438@pucp.edu.pe>
2023-09-21 20:43:57 -0500

    creacioncarpetas

---
+++ src/main/java/com/example/lab06_20206438/Lab0620206438Application.java
@@ -0,0 +1,13 @@
+package com.example.lab06_20206438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0620206438Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab0620206438Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab06_20206438/Lab0620206438ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab06_20206438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0620206438ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
